c7fa0b4165dad9cdb208613792361d93e01c3265
Lázaro <lazaroyaesta@gmail.com>
2022-04-14 15:32:05 +0200

    Add App Component
    This component is the root Component of our Angular application, it defines the root tag: <app-root></app-root> that we use in index.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CoinPerso';
+
+  private roles: string[] = [];
+
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+
+  username?: string;
+
+  constructor(
+    private tokenStorageService: TokenStorageService
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
+
 }
