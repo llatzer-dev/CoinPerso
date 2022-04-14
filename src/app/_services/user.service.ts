b4c543733ca29a76ef809142ff750d2c81ef8db1
Lázaro <lazaroyaesta@gmail.com>
2022-04-14 14:02:54 +0200

    Add Data Service Code
    This service provides methods to access public and protected resources.

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:27017/api/test/'
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getPublicContent(): Observable<any> {
+    return this.http.get(API_URL + 'all', {responseType: 'text'});
+  }
+
 }
