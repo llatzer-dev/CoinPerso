b11cd75b259e884659f9e4c86d8363d48ab147e8
Lázaro <lazaroyaesta@gmail.com>
2022-04-14 14:53:37 +0200

    Update token-storage.service.ts
    Update token-storage.service.ts because I forgot to create the saveUser() method.

--- src/app/_services/token-storage.service.ts
+++ src/app/_services/token-storage.service.ts
@@ -23,6 +23,11 @@ export class TokenStorageService {
     return window.sessionStorage.getItem(TOKEN_KEY);
   }
 
+  public saveUser(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
   public getUser(): any {
     const user = window.sessionStorage.getItem(USER_KEY);
 
