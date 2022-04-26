055eb815899881d96f3b501619968fe051166996
Lázaro <lazaroyaesta@gmail.com>
2022-04-26 10:29:35 +0200

    Update token-storage.service.ts

--- src/app/_services/token-storage.service.ts
+++ src/app/_services/token-storage.service.ts
@@ -11,7 +11,7 @@ export class TokenStorageService {
   constructor() { }
 
   signOut(): void{
-    window.sessionStorage.clear
+    window.sessionStorage.clear();
   }
 
   public saveToken(token: string): void {
