bf7c23b92a93e8a59986bc3b8855bdb46a6bf2f1
Lázaro <lazaroyaesta@gmail.com>
2022-04-30 18:50:25 +0200

    Update Coin.ts

--- src/app/models/Coin.ts
+++ src/app/models/Coin.ts
@@ -1,9 +1,11 @@
 export interface Coin {
   id: string;
+  market_cap_rank: number;
   name: string;
   symbol: string;
   image: string;
   current_price: number;
   price_change_percentage_24h: number;
   total_volume: number;
+  market_cap: number;
 }
