c3b1edf1bbb5daa8aef3ab047f81129ba32ba649
Lázaro <lazaroyaesta@gmail.com>
2022-05-21 15:16:18 +0200

    Add Dialog component & his code

---
+++ src/app/components/portfolio/dialog/dialog.component.ts
@@ -0,0 +1,40 @@
+import { Portfolio } from 'src/app/models/Portfolio';
+import { Movement } from './../../../models/Movement';
+import { Asset } from './../../../models/Asset';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Coin } from 'src/app/models/Coin';
+import { CoinService } from 'src/app/services/coin.service';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  coins: Coin[] = [];
+
+  portfolio: Portfolio = this.data;
+
+  constructor(
+    private coinService: CoinService,
+    public modal: NgbModal,
+    // public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
+
+  ngOnInit(): void {
+    this.modal.open;
+
+    this.coinService.getCoins().subscribe((coinData) => {
+      this.coins = coinData;
+      console.log(coinData)
+      }, (err) => {
+        console.error(err)
+    });
+  }
+
+}
