7c0e22a35907e53274f09e575b3eb3dccc1d1370
Lázaro <lazaroyaesta@gmail.com>
2022-05-28 16:37:36 +0200

    App visuals have been improved
    New styles, css properties, texts & buttons to make the app more user-friendly.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { TokenStorageService } from './../_services/token-storage.service';
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../_services/user.service';
@@ -10,9 +11,11 @@ import { UserService } from '../_services/user.service';
 export class HomeComponent implements OnInit {
 
   content?: string;
+  isLoggedIn = false;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private tokenStorageService: TokenStorageService,
   ) { }
 
   ngOnInit(): void {
@@ -24,6 +27,9 @@ export class HomeComponent implements OnInit {
         this.content = JSON.parse(err.error).message;
       }
     );
+
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
   }
+
 
 }
